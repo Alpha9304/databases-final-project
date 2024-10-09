c51e71d207799dfbb8b2631717f4dad874e54892
Alpha9304 <gbemisoladosu@gmail.com>
2024-10-09 11:22:48 -0400

    done

--- src/main.ts
+++ src/main.ts
@@ -1,138 +1,168 @@
 import "./style.css";
 
-const memeAPI = "https://api.imgflip.com/get_memes";
+//define custom types
+type Meme = {
+  id: string;
+  name: string;
+  url: string;
+  width: Number;
+  height: Number;
+  box_count: Number;
+};
 
-console.log(memeAPI);
+type Data = {
+  memes: Meme[];
+};
+
+type memeAPIResponse = {
+  success: boolean;
+  data: Data;
+};
+
+type cachedResponse = {
+  response: memeAPIResponse | null;
+  date: Date | null;
+};
+
+const memeAPI: string = "https://api.imgflip.com/get_memes";
 
 //get needed document elements
 const memeSection: HTMLElement = document.getElementById("meme") as HTMLElement;
-const topTextInput: HTMLInputElement = document.getElementById("input-top-text") as HTMLInputElement;
-const bottomTextInput: HTMLInputElement = document.getElementById("input-bottom-text") as HTMLInputElement;
+const topTextInput: HTMLInputElement = document.getElementById(
+  "input-top-text",
+) as HTMLInputElement;
+const bottomTextInput: HTMLInputElement = document.getElementById(
+  "input-bottom-text",
+) as HTMLInputElement;
 const errorText: HTMLElement = document.getElementById("error") as HTMLElement;
-const goButton: HTMLButtonElement = document.getElementById("submit") as HTMLButtonElement;
+const goButton: HTMLButtonElement = document.getElementById(
+  "submit",
+) as HTMLButtonElement;
 
-//define custom types
-type Meme = {
-    id: string;
-    name: string;
-    url: string;
-    width: Number;
-    height: Number;
-    box_count: Number;
+//create API response cache
+const cache: cachedResponse = { response: null, date: null };
+
+//function to clear input boxes
+function clearInputBoxes(): void {
+  topTextInput.value = "";
+  bottomTextInput.value = "";
 }
 
-type Data = {
-    memes: Meme[];
-}
-type memeAPIResponse = {
-    success: boolean;
-    data: Data;
-}
-
-function clearInputBoxes(){
-    topTextInput.value = "";
-    bottomTextInput.value = "";
-}
-
-function createMemeText(which: string): HTMLElement{
-    const text: HTMLElement = document.createElement("h2");
-    text.classList.add("absolute");
-    text.classList.add("text-xl");
-    text.classList.add("left-1/2");
-    if(which === "top") {
-        if(topTextInput.value === ""){
-            throw new Error("Please enter text into both fields.");
-        } else {
-            text.classList.add("top-0");
-            text.innerText = topTextInput.value;
-        }
+//helper function to create meme text
+function createMemeText(which: string): HTMLElement {
+  const text: HTMLElement = document.createElement("h2");
+  text.classList.add("absolute");
+  text.classList.add("text-xl");
+  text.classList.add("left-1/2");
+  text.classList.add("transform", "-translate-x-1/2");
+  text.classList.add("text-5xl", "text-white", "font-extrabold", "text-shadow");
+  if (which === "top") {
+    if (topTextInput.value === "") {
+      throw new Error("Please enter text into both fields.");
     } else {
-        if(bottomTextInput.value === ""){
-            throw new Error("Please enter text into both fields.");
-        } else {
-            text.classList.add("bottom-0");
-            text.innerText = bottomTextInput.value;
-        }
+      text.classList.add("top-0");
+      text.innerText = topTextInput.value.toUpperCase();
     }
+  } else {
+    if (bottomTextInput.value === "") {
+      throw new Error("Please enter text into both fields.");
+    } else {
+      text.classList.add("bottom-0");
+      text.innerText = bottomTextInput.value.toUpperCase();
+    }
+  }
 
-    return text;
+  return text;
 }
 
+//helper function to fetch data from the API
+async function getResponse(): Promise<memeAPIResponse> {
+  const response: Response = await fetch(memeAPI);
+  const data: memeAPIResponse = await response.json();
+  return data;
+}
+
+//helper function to get the meme and use the cache if a day has not passed
+async function getMeme(): Promise<memeAPIResponse> {
+  const today: Date = new Date();
+  if (cache.response === null || cache.date === null) {
+    const data: memeAPIResponse = await getResponse();
+    cache.response = data;
+    cache.date = today;
+    return data;
+  } else {
+    if (today.getTime() < cache.date.getTime() + 60 * 60 * 24 * 1000) {
+      return cache.response;
+    } else {
+      const data: memeAPIResponse = await getResponse();
+      cache.response = data;
+      cache.date = today;
+      return data;
+    }
+  }
+}
+
+//function to generate a new meme
+async function generateMeme(): Promise<void> {
+  memeSection.innerHTML = "";
+  errorText.innerText = "";
+  topTextInput.classList.add("border-stone-200");
+  topTextInput.classList.remove("border-red-200");
+
+  bottomTextInput.classList.add("border-stone-200");
+  bottomTextInput.classList.remove("border-red-200");
+
+  try {
+    const memeContainer: HTMLElement = document.createElement("div");
+    memeContainer.classList.add(
+      "relative",
+      "w-full",
+      "h-[600px]",
+      "bg-contain",
+      "bg-[length:auto_100%]",
+      "bg-no-repeat",
+      "bg-center",
+    );
+
+    const result: memeAPIResponse = await getMeme();
+
+    const choice: number = Math.floor(Math.random() * result.data.memes.length);
+    const chosenImgURL: string = result.data.memes[choice].url;
+
+    memeContainer.style.backgroundImage = `url(${chosenImgURL})`;
+    const topText: HTMLElement = createMemeText("top") as HTMLElement;
+    const bottomText: HTMLElement = createMemeText("bottom") as HTMLElement;
+    memeContainer.appendChild(topText);
+    memeContainer.appendChild(bottomText);
+    memeContainer.classList.add("bg-transparent");
+    memeSection.classList.add("bg-black");
+    memeSection.appendChild(memeContainer);
+  } catch (error) {
+    errorText.innerText = (error as Error).message;
+
+    if (topTextInput.value === "") {
+      topTextInput.classList.remove("border-stone-200");
+      topTextInput.classList.add("border-red-200");
+    }
+
+    if (bottomTextInput.value === "") {
+      bottomTextInput.classList.remove("border-stone-200");
+      bottomTextInput.classList.add("border-red-200");
+    }
+  }
+}
+
+//function to generate a meme on enter
 function generateOnEnter(event: KeyboardEvent): void {
-    if(event.key === "Enter"){
-        generateMeme();
-    }
+  if (event.key === "Enter") {
+    generateMeme();
+  }
 }
 
-async function generateMeme() { //function typing?
-    memeSection.innerHTML = "";
-    errorText.innerText = "";
-    topTextInput.classList.add("border-stone-200");
-    topTextInput.classList.remove("border-red-200");
-
-    bottomTextInput.classList.add("border-stone-200");
-    bottomTextInput.classList.remove("border-red-200");
-    
-    try {
-        
-        const memeContainer : HTMLElement = document.createElement("div");
-        memeContainer.classList.add("relative", "w-full", "h-[600px]", "bg-contain", "bg-cover");
-        
-
-        const response : Response = await fetch('https://api.imgflip.com/get_memes');
-        const result: memeAPIResponse = await response.json();
-        
-        const choice = Math.floor((Math.random() * result.data.memes.length));
-        const chosenImgURL = result.data.memes[choice].url;
-
-        memeContainer.style.backgroundImage = `url(${chosenImgURL})`;
-        memeContainer.classList.add("bg-black");
-
-        //const textContainer : HTMLElement = document.createElement("div");
-        const topText: HTMLElement = createMemeText("top") as HTMLElement;
-        const bottomText: HTMLElement = createMemeText("bottom") as HTMLElement;
-        memeContainer.appendChild(topText);
-        memeContainer.appendChild(bottomText);
-        memeContainer.classList.add("bg-transparent");
-
-        //memeContainer.appendChild(memeContainer);
-        memeSection.appendChild(memeContainer);
-        
-        /*
-        const topText: HTMLElement = createMemeText("top") as HTMLElement;
-        const bottomText: HTMLElement = createMemeText("bottom") as HTMLElement;
-        memeSection.appendChild(topText);
-        memeSection.appendChild(bottomText);
-        
-        const response : Response = await fetch('https://api.imgflip.com/get_memes');
-        const result: memeAPIResponse = await response.json();
-        
-        const choice = Math.floor((Math.random() * result.data.memes.length));
-        const chosenImgURL = result.data.memes[choice].url;
-
-        const imageSection : HTMLImageElement = document.createElement("img");
-        imageSection.src = chosenImgURL;
-
-        memeSection.appendChild(imageSection);*/
-    } catch (error) {
-        errorText.innerText = (error as Error).message;
-        
-        if(topTextInput.value === ""){
-            topTextInput.classList.remove("border-stone-200");
-            topTextInput.classList.add("border-red-200");
-        }
-
-        if(bottomTextInput.value === ""){
-            bottomTextInput.classList.remove("border-stone-200");
-            bottomTextInput.classList.add("border-red-200");
-        }
-    }
-}
-
+//add needed event listeners
 goButton.addEventListener("click", generateMeme);
 topTextInput.addEventListener("keydown", generateOnEnter);
 bottomTextInput.addEventListener("keydown", generateOnEnter);
 
 //clear input boxes on reload
 clearInputBoxes();
-    
